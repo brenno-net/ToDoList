b818f804140b14e0e9ce1019cb2f4e17fa283592
Brenno Pereira <brennopereira007@gmail.com>
2023-10-06 23:26:18 -0300

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+function App() {
+  return (
+    <div className="App">
+      
+        <div className="container">
+
+        <h1>ToDoApp</h1>
+
+          <div className="top">
+          <input type= "text" placeholder="Adicionar Tarefa" />
+            
+          </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
