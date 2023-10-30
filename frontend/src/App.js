19e79edd4e867c18210bf52e11672c3a7e015386
Brenno Pereira <brennopereira007@gmail.com>
2023-10-30 17:33:10 -0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,74 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import ToDo from '../src/components/ToDo';
-import { getAllToDo, addToDo, updateToDo, deleteToDo  } from './utils/HandleApi';
-
+import React, { useEffect, useState } from "react";
+import ToDo from "../src/components/ToDo";
+import { getAllToDo, addToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
+import Pomodoro from "./components/Pomodoro";
 
 function App() {
-
-  const [toDo,setToDo] = useState([])
-  const [text, setText] = useState("")
-  const [isUpdating, setIsUpdating] = useState(false)
-  const [toDoId, setToDoId] = useState("")
-  
-
-
-   
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [toDoId, setToDoId] = useState("");
 
   useEffect(() => {
-    getAllToDo(setToDo)
-  }, [])
+    getAllToDo(setToDo);
+  }, []);
 
-const updateMode  = (_id, text) => {
-  setIsUpdating(true)
-  setText(text)
-  setToDoId(_id)
-}
+  const updateMode = (_id, text) => {
+    setIsUpdating(true);
+    setText(text);
+    setToDoId(_id);
+  };
 
   return (
     <div className="App">
-
       <div className="container">
-
         <h1>Nome Do App</h1>
 
         <div className="top">
-
-          <input type="text"
-          placeholder="Adicionar Tarefa" 
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          maxLength={46}
+          <input
+            type="text"
+            placeholder="Adicionar Tarefa"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            maxLength={46}
           />
 
-          <div 
-          className="add" 
-          onClick={ isUpdating ? 
-            () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating) 
-          : () => addToDo(text, setText, setToDo)}>
+          <div
+            className="add"
+            onClick={
+              isUpdating
+                ? () =>
+                    updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+                : () => addToDo(text, setText, setToDo)
+            }
+          >
             {isUpdating ? "Update" : "Adicionar"}
-
           </div>
-
         </div>
 
         <div className="list">
-
-          {toDo.map((item) => <ToDo 
-          key={item._id} 
-          text={item.text}
-          isChecked={item.isChecked}
-          updateMode = {() => updateMode(item._id, item.text)}
-          deleteToDo={() => deleteToDo(item._id, setToDo)}/>)}
-          
-
+          {toDo.map((item) => (
+            <ToDo
+              key={item._id}
+              text={item.text}
+              isChecked={item.isChecked}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteToDo={() => deleteToDo(item._id, setToDo)}
+            />
+          ))}
         </div>
-
+        
       </div>
-
+      <div className="pomodoro-box">
+          <Pomodoro />
+        </div>
     </div>
-
   );
-
 }
 
 export default App;
---
+++ frontend/src/components/Pomodoro.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+function Pomodoro() {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        } else if (minutes > 0) {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        } else {
+          clearInterval(interval);
+          setIsActive(false);
+          // Você pode adicionar uma notificação aqui quando o tempo acabar
+        }
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, minutes, seconds]);
+
+  const toggleTimer = () => {
+    setIsActive(!isActive);
+  };
+
+  const resetTimer = () => {
+    setIsActive(false);
+    setMinutes(25);
+    setSeconds(0);
+  };
+
+  return (
+    <div className="pomodoro">
+      <h1>Pomodoro Timer</h1>
+      <div className="timer">
+        <p>
+          {minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
+        </p>
+      </div>
+      <div className="controls">
+        <button onClick={toggleTimer}>{isActive ? 'Pause' : 'Start'}</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Pomodoro;
